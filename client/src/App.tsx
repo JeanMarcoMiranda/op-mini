817edc6d5035f98420c91394386dbe376ad7365a
BrucceV <brucce.villena@tecsup.edu.pe>
2021-05-24 16:43:20 -0500

    [W-01] Cambios estructura de las carpetas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { appRoutes } from './routes/Routes'
+import { appRoutes } from './routes'
 
 import './App.css';
 
---
+++ client/src/components/hooks/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+//import Example from "./example";
+//import Example2 from "./example";
+
+//export { Example, Example2 }
