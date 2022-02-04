7f822f12e194b1586335f8437999d39a45d1a884
JeanMarcoMiranda <elecord2014@gmail.com>
2022-02-03 19:55:51 -0500

    [J-12] Mejorando componente de paginación

--- client/src/components/common/Table/Pagination.tsx
+++ client/src/components/common/Table/Pagination.tsx
@@ -8,16 +8,26 @@ interface IPaginationProps {
 }
 
 const Pagination: React.FC<IPaginationProps> = ({ fieldsPerPage, totalFields, paginate, currentPage }) => {
-    const pageNumbers = []
+    const totalPageNumbers = []
     const styleCurrentPage = "z-10 bg-indigo-50 border-indigo-500 text-indigo-600"
     const styleNormalPage = "bg-white border-gray-300 text-gray-500 hover:bg-gray-50"
 
+    let pagesToShow = []
+
     for (let i = 1; i <= Math.ceil(totalFields / fieldsPerPage); i++) {
-        pageNumbers.push(i)
+        console.log("buebas")
+        totalPageNumbers.push(i)
+    }
+
+    if (currentPage <= 6) {
+        const pagesLength = totalPageNumbers.length
+        pagesToShow = totalPageNumbers.slice(0, pagesLength > 9 ? 9 : pagesLength)
+    } else {
+        pagesToShow = totalPageNumbers.slice(currentPage - 6, currentPage + 4)
     }
 
     return <nav className="inline-flex rounded-md shadow-sm -space-x-px mt-6">
-        {pageNumbers.map(number => (
+        {pagesToShow.map(number => (
             <a onClick={() => paginate(number)} key={number} href="#" className={`${number === currentPage ? styleCurrentPage : styleNormalPage} relative inline-flex items-center px-4 py-2 border text-sm font-medium`}>
                 {number}
             </a>
